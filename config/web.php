510dbcae4a8ecc5c4eded1f43a371d7e69e79b07
Igor Zahvatov <iz@nowmedia.ru>
2018-12-24 22:49:03 +0300

    partial

--- config/web.php
+++ config/web.php
@@ -26,6 +26,15 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
         'db' => $db,
     ],
     'params' => $params,
